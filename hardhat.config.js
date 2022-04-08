1e4cc4bd9bdf1dab93b3ab15e3967c9df1565be9
kiyimba1 <edwineddie54@gmail.com>
2022-04-08 23:48:13 +0300

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,27 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/gkECkEGoobCww3Go1djP8I5uUgHVZdaD",
+      accounts: ["f041aed8af124673f267aa4b73f5197fdaa5a3d2a5eed0c0c8308e8ef8b1af24"]
+    }
+  }
+};
---
+++ scripts/run.js
@@ -0,0 +1,37 @@
+
+
+const main = async () => {
+    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+    const waveContract = await waveContractFactory.deploy();
+    await waveContract.deployed();
+
+    console.log("Contract deployed to:", waveContract.address);
+
+
+    let waveCount;
+    waveCount = await waveContract.getTotalWaves();
+
+    let waveTxn = await waveContract.wave("A message!");
+    await waveTxn.wait();
+
+    waveCount = await waveContract.getTotalWaves();
+
+    const [_, randomPerson] = await hre.ethers.getSigners();
+    waveTxn = await waveContract.connect(randomPerson).wave("Another message!");
+    await waveTxn.wait();
+
+    let allWaves = await waveContract.getAllWaves();
+    console.log(allWaves);
+};
+
+const runMain = async () => {
+    try {
+        await main();
+        process.exit(0);
+    } catch(error) {
+        console.log(error);
+        process.exit(1)
+    }
+};
+
+runMain();
